79c7f41952e023ab64256243044cc4272d9051c3
Matt Parker <parker20121@gmail.com>
2016-05-26 14:19:24 -0400

    First two instagram datasets

---
+++ data/instagram/instagram_yemen_user_posts.rb
@@ -0,0 +1,36 @@
+require 'date'
+require 'json'
+
+file = File.open('instagram_yemen_user_posts.csv','w')
+
+file << "instagram_id, epoch, date_time, location_id, location_name, location_latitude, location_longitude\n"
+
+row =  0
+
+open('instagram_yemen_user_posts','r').each do |line|
+
+	data = JSON.parse( line )
+
+	id = data['_id']
+	epoch = data["_source"]["created_time"]
+	location = data["_source"]["location"]
+		
+	location_id = ''
+	location_latitude = ''
+	location_longitude = ''
+	location_name = ''
+
+	unless data["_source"]["location"].nil?
+		location_id = data["_source"]["location"]["id"]
+      location_name = data["_source"]["location"]["name"]
+      location_latitude = data["_source"]["location"]["latitude"]
+      location_longitude = data["_source"]["location"]["longitude"]
+	end
+
+	file << "#{id},#{epoch},#{Time.at(epoch.to_i)},#{location_id},#{location_name},#{location_latitude},#{location_longitude}\n"
+
+	row = row + 1
+
+	print "#{row}\n" if row % 50000 == 0
+
+end
